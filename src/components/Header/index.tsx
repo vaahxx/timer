74a1718f7c27c3a158cee6bebe027cb8923e36d0
Valentina Garavaglia <vaahxx@gmail.com>
2022-12-11 15:41:33 -0500

    finish Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,20 @@
+import { HeaderContainer } from "./styles";
+import { Timer, Scroll } from "phosphor-react";
+import { NavLink } from "react-router-dom";
+import logo from "../../assets/logo-ignite.svg";
+
 export function Header() {
-  return <h1>Header</h1>;
+  return (
+    <HeaderContainer>
+      <img src={logo} alt='' />
+      <nav>
+        <NavLink to='/' title='Timer'>
+          <Timer size={24} />
+        </NavLink>
+        <NavLink to='/history' title='History'>
+          <Scroll size={24} />
+        </NavLink>
+      </nav>
+    </HeaderContainer>
+  );
 }
