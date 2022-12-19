e9b2a70f857e4ddd709aa2ad00ed560d3e4bdb64
Valentina Garavaglia <vaahxx@gmail.com>
2022-12-19 16:37:56 -0500

    create Status component

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from "./styles";
+import { HistoryContainer, HistoryList, Status } from "./styles";
 
 export function History() {
   return (
@@ -18,28 +18,36 @@ export function History() {
 
           <tbody>
             <tr>
-              <td>Task</td>
+              <td>Task Name</td>
               <td>20 minutes</td>
               <td>2 months ago</td>
-              <td>Concluded</td>
+              <td>
+                <Status statusColor='yellow'>In progress</Status>
+              </td>
             </tr>
             <tr>
-              <td>Task</td>
+              <td>Task Name</td>
               <td>20 minutes</td>
               <td>2 months ago</td>
-              <td>Concluded</td>
+              <td>
+                <Status statusColor='yellow'>In progress</Status>
+              </td>
             </tr>
             <tr>
-              <td>Task</td>
+              <td>Task Name</td>
               <td>20 minutes</td>
               <td>2 months ago</td>
-              <td>Concluded</td>
+              <td>
+                <Status statusColor='red'>Interrupted</Status>
+              </td>
             </tr>
             <tr>
-              <td>Task</td>
+              <td>Task Name</td>
               <td>20 minutes</td>
               <td>2 months ago</td>
-              <td>Concluded</td>
+              <td>
+                <Status statusColor='green'>Done</Status>
+              </td>
             </tr>
           </tbody>
         </table>
