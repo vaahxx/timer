42f098dc74d827f3962feca7b6001bac34f38e94
Valentina Garavaglia <vaahxx@gmail.com>
2023-06-16 13:20:28 -0400

    change css

--- src/layouts/DefautLayout/styles.ts
+++ src/layouts/DefautLayout/styles.ts
@@ -2,7 +2,6 @@ import styled from "styled-components";
 
 export const LayoutContainer = styled.div`
   max-width: 74rem;
-  height: calc(100vh - 10rem);
   margin: 5rem auto;
   padding: 2.5rem;
   background: ${(props) => props.theme["gray-800"]};
